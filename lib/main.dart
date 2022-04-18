858c245d5d98b7296ef6fbe6a0a74a8f8064fd42
@manuelduarte077 <carlosd99m@gmail.com>
2022-04-18 11:17:14 -0600

    configuration of packages and flutterfire (firebase) :fire:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:game_app/app/app.dart';
 
-void main() {
+import 'package:game_app/app/app.dart';
+import 'package:game_app/firebase_options/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
